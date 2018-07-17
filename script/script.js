ec04fd854adac0c602bf5a60a99127c2b276273e
tlacolley <thomaslacolley@gmail.com>
2018-07-17 16:56:32 +0200

    Creation class out::before for produit sold out

--- script/script.js
+++ script/script.js
@@ -5,9 +5,9 @@ console.log("nioevnio");
 for (var i = 0; i < 18 ; i++) {
 
     var htmlArt = `<div class="col-sm-6 col-md-3 col-lg-2">
-        <div class="card">
-            <a href="#"><img class="card-img-top" src="https://picsum.photos/200/300/?random" alt="Card image cap"></a>
-            <div class="card-body">
+        <div class="card out">
+            <a href="#"><img class="card-img-top " src="https://picsum.photos/200/300/?random" alt="Card image cap"></a>
+            <div class="card-body ">
                 <ul>
                     <li><a href="#" class="btn-cards"> BUY </a></li>
                     <li><a href="#"><h3 class="">title</h3></a></li>
